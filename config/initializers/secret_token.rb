ce07d21e376e7e16b4fba6f995e0739ef7f75e71
Chris Brulak <ccmbrulak@gmail.com>
2010-09-27 23:46:27 -0600

    adding rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Multisignintest::Application.config.secret_token = 'a1ac5efb7f13dd4a9160ba02e7bce6088474435377214a9e670d83a8ac8a41a5fb0344a095455945ee0d543aa27b7b53ec1498546938904d5bbe8e27f2fd5a33'
